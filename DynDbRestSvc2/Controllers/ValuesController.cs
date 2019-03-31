bc1f28baf531dceb12bef2b94682ea469c3f8578
shrinik <24953626+shrinik@users.noreply.github.com>
2019-03-31 06:40:50 -0500

    Add project files.

---
+++ DynDbRestSvc2/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DynDbRestSvc2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static IAmazonDynamoDB DynamoDBClient { get; set; }
+
+        public ValuesController(IAmazonDynamoDB dynamoDBClient)
+        {
+            DynamoDBClient = dynamoDBClient;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            return await GetDataFromDb();
+        }
+
+        private static async Task<string> GetDataFromDb()
+        {
+            var table = Table.LoadTable(DynamoDBClient, "policy");
+            var item = await table.GetItemAsync("98722280");
+            return item.ToJsonPretty();
+        }
+    }
+}
